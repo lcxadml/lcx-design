2e5ee9fa1d043f77728568a6280bf617c84f22ad
licunxing <864255598@qq.com>
2024-12-22 22:28:45 +0800

    feat: update icon script

--- scripts/publish-icon.sh
+++ scripts/publish-icon.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 set -e
-echo "pnpm update:version"
+echo "pnpm update:version..."
 pnpm update:icon-version
 
+echo "update version success!"
+
+echo "build:icon..."
+pnpm run build:icon
+echo "build:icon success!"
 cd packages/lcx-design-icon/dist/lcx-design-icon
 npm publish
 cd -

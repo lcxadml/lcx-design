e83e9d88bd80905c1a9c9ad4e248b5babd8ffa7c
licunxing <864255598@qq.com>
2024-12-22 22:37:11 +0800

    feat: update icon script

--- scripts/publish-icon.sh
+++ scripts/publish-icon.sh
@@ -3,12 +3,12 @@
 set -e
 echo "pnpm update:version..."
 pnpm update:icon-version
-
 echo "update version success!"
 
 echo "build:icon..."
 pnpm run build:icon
 echo "build:icon success!"
+
 cd packages/lcx-design-icon/dist/lcx-design-icon
 npm publish
 cd -
--- scripts/update-version.ts
+++ scripts/update-version.ts
@@ -1,12 +1,12 @@
 import { readFile, writeFile } from "fs/promises";
 import path from "path";
-import { lcxDesignRoot } from "../internal/build/build-utils";
+import { IconOut, lcxDesignIconRoot } from "../internal/build/build-utils";
 import chalk from "chalk";
 
 const main = async () => {
   const tagVersion = process.env.TAG_VERSION;
   const dataStr = await readFile(
-    path.resolve(lcxDesignRoot, "package.json"),
+    path.resolve(lcxDesignIconRoot, "package.json"),
     "utf-8"
   );
   let data;
@@ -28,7 +28,7 @@ const main = async () => {
   data.version = publishVersion;
 
   await writeFile(
-    path.resolve(lcxDesignRoot, "package.json"),
+    path.resolve(IconOut, "lcx-design-icon", "package.json"),
     JSON.stringify(data, null, 2)
   );
   console.log("update icon version successful!", data?.version);
